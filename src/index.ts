a4ac2a412901d99afcd02584d1ddb6e33dda952e
Arttu Pennanen <arttu@pennanen.org>
2021-12-22 01:24:05 +0200

    Smart logging

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,52 @@ const getWords = async () => {
         return _words
     }
 
-    const file = await fs.readFile('./words.txt', 'utf8')
+    const file = await fs.readFile('/Users/arttupennanen/Desktop/projects/word/words.txt', 'utf8')
     _words = file.split('\n')
     return _words
 }
 
 async function main(argv: string[]) {
     const args = argv.slice(2)
-
     if (!args[0]) return console.log('hirsibuu <state> [invalid]')
 
     const words = await getWords()
-
     const [solveState, invalidCharacters = ''] = args
-    const notPossibleString = [...new Set(solveState.replaceAll('?', '').concat(invalidCharacters).split(''))].join('')
 
-    const regexp = new RegExp(`^${solveState.replaceAll('?', `[^${notPossibleString}]`)}$`)
+    const prior = [...new Set(solveState.replace(/[\?\-\_\.\,\s]/g, '').split(''))].join('')
+
+    const invalid = [
+        ...new Set(
+            solveState
+                .replace(/[\?\-\_\.\,\s]/g, '')
+                .concat(invalidCharacters)
+                .split('')
+        )
+    ].join('')
+
+    const regexp = new RegExp(`^${solveState.replace(/[\?\-\_\.\,\s]/g, `[^${invalid}]`)}$`)
+
     const matches = words.filter((w) => regexp.test(w))
+    const quantities: { [char: string]: number } = {}
 
     for (const [index, word] of matches.entries()) {
-        console.log(index, ':', word)
+        const unlisted = new Set(word.replace(new RegExp(`[${prior}]`, 'g'), '').split(''))
+
+        unlisted.forEach((c) => (c in quantities ? quantities[c]++ : (quantities[c] = 1)))
+
+        if (index < 5) {
+            console.log(index, ':', word)
+        }
+    }
+
+    console.log('')
+
+    const mostOccuring = Object.entries(quantities)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 5)
+
+    for (const [char, amount] of mostOccuring) {
+        console.log(char, 'in', amount, 'of', matches.length, 'words')
     }
 }
 
