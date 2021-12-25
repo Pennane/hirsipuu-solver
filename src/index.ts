10e7f45285241b2832da8fb7be678f2f23476d86
Arttu Pennanen <arttu@pennanen.org>
2021-12-25 20:05:52 +0200

    Calculate char worth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
 import fs from 'fs/promises'
 import path from 'path'
 
+const occuranceInString = (character: string, word: string): number => {
+    return word.split(character).length - 1
+}
+
+export const worth = {
+    a: 1,
+    k: 2,
+    o: 1,
+    n: 2,
+    e: 1,
+    s: 2,
+    l: 2,
+    i: 1,
+    t: 2,
+    u: 1,
+    j: 2,
+    ä: 1,
+    r: 2,
+    y: 3,
+    m: 2,
+    h: 2,
+    p: 2,
+    g: 5,
+    v: 2,
+    d: 5,
+    ö: 1,
+    b: 5,
+    c: 5,
+    f: 5,
+    z: 5,
+    w: 5,
+    q: 5,
+    x: 5
+}
+
 let _words: string[]
 
 const getWords = async () => {
@@ -34,12 +69,21 @@ async function main(argv: string[]) {
     const regexp = new RegExp(`^${wordState.replace(/[\?\-\_\.\,\s]/g, `[^${invalid}]`)}$`)
 
     const matches = words.filter((w) => regexp.test(w))
+    const occurrances: { [char: string]: number } = {}
     const quantities: { [char: string]: number } = {}
 
     for (const [index, word] of matches.entries()) {
         const unlisted = new Set(word.replace(new RegExp(`[${appeared}]`, 'g'), '').split(''))
 
-        unlisted.forEach((c) => (c in quantities ? quantities[c]++ : (quantities[c] = 1)))
+        unlisted.forEach((c) => {
+            if (c in occurrances) {
+                occurrances[c]++
+                quantities[c] += occuranceInString(c, word)
+            } else {
+                occurrances[c] = 1
+                quantities[c] = occuranceInString(c, word)
+            }
+        })
 
         if (index < 5) {
             console.log(index, ':', word)
@@ -48,12 +92,32 @@ async function main(argv: string[]) {
 
     console.log('')
 
-    const mostOccuring = Object.entries(quantities)
-        .sort((a, b) => b[1] - a[1])
+    const mostOccuring = Object.entries(occurrances)
+        .sort((a, b) => {
+            if (a[1] === b[1]) {
+                return (
+                    (worth[b[0] as keyof typeof worth] * quantities[b[0]]) / b[1] -
+                    (worth[a[0] as keyof typeof worth] * quantities[a[0]]) / a[1]
+                )
+            }
+
+            return b[1] - a[1]
+        })
         .slice(0, 5)
 
-    for (const [char, amount] of mostOccuring) {
-        console.log(char, 'in', amount, 'of', matches.length, 'words')
+    for (const [c, a] of mostOccuring) {
+        const k = c as keyof typeof worth
+        console.log(
+            c,
+            'in',
+            a,
+            'of',
+            matches.length,
+            'words',
+            `(≈ ${(quantities[c] / a).toFixed(2)} pc or ≈ ${Math.round(
+                (quantities[c] / a) * (worth.hasOwnProperty(k) ? worth[k] : 1)
+            )} points)`
+        )
     }
 }
 
